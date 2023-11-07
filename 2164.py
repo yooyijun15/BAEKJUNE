b23359fa465121084c9e9b178910e4ab0362f443
yooyijun15 <uyijune15@gmail.com>
2023-11-08 01:36:25 +0900

    20231107

---
+++ 2164.py
@@ -0,0 +1,7 @@
+from collections import deque
+N = int(input())
+dq = deque(range(1,N+1))
+while len(dq) >1:
+    dq.popleft()
+    dq.append(dq.popleft())
+print(dq.popleft())
---
+++ 9012.py
@@ -0,0 +1,15 @@
+for _ in range(int(input())):
+    stk = []
+    isVPS = True
+    for ch in input():
+        if ch == '(':
+            stk.append(ch)
+        else:
+            if stk:
+                stk.pop()
+            else:
+                isVPS = False
+                break
+    if stk:
+        isVPS = False
+    print('Yes' if isVPS else 'No')
